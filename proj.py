f72008e5fa905a8354990904ae3bc18a3ade8e07
Fei Yuan <yuan@nscl.msu.edu>
2015-07-07 12:27:14 -0700

    Put everything together sort of

--- proj.py
+++ proj.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python
 import numpy as np
 import matplotlib.pyplot as plt
-import pandas as pd
 import scipy.integrate as spi
 import scipy.optimize as spo
-from ad            import adnumber
-from numpy         import arcsinh, sqrt, nan, pi
+from ad import adnumber                 # for automatic differentation
+from numpy import abs, arcsinh, sqrt, min, max, nan, pi
 
 def cbrt(x):
     '''Cube root.  Needed to avoid getting complex numbers and also for
@@ -14,8 +13,15 @@ def cbrt(x):
         return x**(1./3.)
     return -(-x)**(1./3.)
 
-def pressure(e_pn, e_e, mu_n, mu_p, mu_e, n_n, n_p, n_e):
-    return -e_pn - e_e + mu_n * n_n + mu_p * n_p + mu_e * n_e
+def total_pressure(n_n):
+    n_e = n_p
+    n_p = solve_for_proton_number_density(n_n)
+    epsilon_np = nucleon_energy_density(n_n, n_p)
+    epsilon_e  = electron_energy_density(n_e)
+    mu_n = proton_chemical_potential(n_n, n_p)
+    mu_p = neutron_chemical_potential(n_n, n_p)
+    mu_e = electron_chemical_potential(n_e)
+    return mu_n * n_n + mu_p * n_p + mu_e * n_e - epsilon_np - epsilon_e
 
 def nucleon_interaction_density(n_n, n_p):
     '''[V_np] Interaction using mean-field Skyrme model.  Eq (28)'''
@@ -52,15 +58,12 @@ def plot_energy_density():
     pure neutron matter'''
     symmetric    = lambda n_B: nucleon_energy_density(n_B * .5, n_B * .5)
     pure_neutron = lambda n_B: nucleon_energy_density(n_B, 0)
-    n_B = np.linspace(0, 2 * .16, 250)
-    plt.plot(n_B / .16,    symmetric(n_B) / n_B * MEV_FM, "k")
-    plt.plot(n_B / .16, pure_neutron(n_B) / n_B * MEV_FM, "b")
+    n_B = np.linspace(0, 2 * N_0, 250)
+    plt.plot(n_B / N_0,    symmetric(n_B) / n_B * MEV_FM, "k")
+    plt.plot(n_B / N_0, pure_neutron(n_B) / n_B * MEV_FM, "b")
     plt.xlabel("n / n_saturation")
     plt.ylabel("epsilon / n_B  /MeV")
     plt.show()
-
-def vectorize(f):
-    return np.vectorize(f, [float])
 
 @np.vectorize
 def proton_chemical_potential(n_n, n_p):
@@ -72,21 +75,31 @@ def neutron_chemical_potential(n_n, n_p):
     n_n = adnumber(n_n)
     return nucleon_energy_density(n_n, n_p).d(n_n)
 
-def fermi_gas_chemical_potential(n, m):
-    '''Chemical potential of a noninteracting, relativistic Fermi gas.'''
-    k = number_density_to_momentum(n)
+def ideal_fermi_gas_energy_density(k, m):
+    '''Energy density of an ideal Fermi gas.'''
+    mu = fermi_gas_chemical_potential(k, m)
+    return (k * mu * (k**2 + mu**2) - m**4 * arcsinh(k / m)) / (8. * pi**2)
+
+def ideal_fermi_gas_chemical_potential(k, m):
+    '''Chemical potential of an ideal Fermi gas.'''
     return sqrt(m**2 + k**2)
 
-@np.vectorize
-def electron_chemical_potential(n_e):
-    return fermi_gas_chemical_potential(n_e, m=M_E)
+def make_ideal_fermi_gas(m):
+    def epsilon(n):
+        k = number_density_to_momentum(n)
+        return fermi_gas_energy_density(k, m)
+    def mu(n):
+        k = number_density_to_momentum(n)
+        return fermi_gas_chemical_potential(k, m)
+    return epsilon, mu
 
-@np.vectorize
-def muon_chemical_potential(n_e):
-    return fermi_gas_chemical_potential(n_e, m=M_MU)
+electron_energy_density, electron_chemical_potential \
+    = make_ideal_fermi_gas(M_E)
+muon_energy_density, muon_chemical_potential \
+    = make_ideal_fermi_gas(M_MU)
 
-def solve_equation(equation, x0):
-    x, _, ier, _ = spo.fsolve(equation, x0=x0, full_output=True)
+def solve_equation(equation, **kwargs):
+    x, _, ier, _ = spo.fsolve(equation, full_output=True, **kwargs)
     return x if ier == 1 else nan       # return NaN if solution not found
 
 def beta_equilibrium_equation(n_n, n_p):
@@ -96,15 +109,15 @@ def beta_equilibrium_equation(n_n, n_p):
 
 def solve_for_neutron_number_density(n_p):
     equation = lambda n_n: beta_equilibrium_equation(n_n, n_p)
-    n_n = np.linspace(1e-10, 10 * .16, 250)##
-    plt.plot(n_n, equation(n_n))##
+    # n_n = np.linspace(1e-10, 10 * N_0, 250)##
+    # plt.plot(n_n, equation(n_n))##
     return solve_equation(equation, x0=1.)
 
 def solve_for_proton_number_density(n_n):
     equation = lambda n_p: beta_equilibrium_equation(n_n, n_p)
-    n_p = np.linspace(1e-5, 100 * .16, 250)##
-    plt.plot(n_p, [equation(n_p_) for n_p_ in n_p])##
-    return solve_equation(equation, x0=1.)
+    # n_p = np.linspace(1e-5, 100 * N_0, 250)##
+    # plt.plot(n_p, [equation(n_p_) for n_p_ in n_p])##
+    return solve_equation(equation, x0=1)
 
 # note: energy-like quantities are in units of 1/fm, equivalent to 197.33 MeV
 MEV_FM = 197.33
@@ -122,44 +135,49 @@ T_2 =  -.137 # fm**4
 T_3 = 47.29  # fm**5
 X_0 =   .34  # (dimensionless)
 
-plt.gca().axhline(y=0, color="gray")
+def create_figure(nx=1, ny=1):
+    figure = plt.figure()
+    axes = [figure.add_subplot(nx, ny, i + 1) for i in range(nx * ny)]
+    return figure, axes
 
-# note: it seems that only one of the roots is physical (nonnegative num
-# density) perhaps we should solve n_e and n_p first (resp.) and figure out
-# where they cross the x-axis?
-nvar = np.linspace(1e-5, 4, 100) * .16
-n_n = nvar
-n_p = .07 * .16
-plt.xlabel("n_n")
-plt.plot(n_p, electron_chemical_potential(n_p), label="e")
-plt.plot(nvar/.16, proton_chemical_potential(n_n, n_p), label="p")
-plt.plot(nvar/.16, neutron_chemical_potential(n_n, n_p), label="n")
-plt.plot(nvar/.16, beta_equilibrium_equation(n_n, n_p), label="beta")
-plt.legend()
+def plot_map(figure, axes, func, xmin, xmax, ymin, ymax,
+             xnum=50, ynum=50, **kwargs):
+    xs, ys = np.meshgrid(np.linspace(xmin, xmax, xnum),
+                         np.linspace(ymin, ymax, ynum))
+    zs = func(xs, ys)
+    return axes.imshow(zs, origin="lower",
+                       extent=(xmin, xmax, ymin, ymax), **kwargs)
+
+def plot_against_number_densities(figure, axes, func):
+    '''Plot a function of (n_n, n_p) where number densities are in 1/fm**3.'''
+    def f(n_n, n_p):
+        return abs(func(n_n * N_0, n_p * N_0))
+    img = plot_map(figure, axes, f,
+                   xmin=1e-5, xmax=6, ymin=1e-5, ymax=6, xnum=100, ynum=100,
+                   cmap="afmhot", interpolation="none", vmax=1)
+    axes.set_xlabel("n_n /n_0")
+    axes.set_ylabel("n_p /n_0")
+    return img
+
+def plot_beta_equilibrium_solutions():
+    fig = plt.figure()
+    ax1 = fig.add_subplot(121)
+    ax2 = fig.add_subplot(122)
+    def f(n_n, n_p):
+        return np.logical_and(proton_chemical_potential(n_n, n_p) >= 0,
+                              neutron_chemical_potential(n_n, n_p) >= 0)
+    plot_against_number_densities(fig, ax1, f)
+    img = plot_against_number_densities(fig, ax2, beta_equilibrium_equation)
+    fig.colorbar(img)
+
+fig, [ax] = create_figure(1, 1)
+n_ns = np.linspace(1e-5, 6)
+n_ps = [solve_for_proton_number_density(N_0 * n_n) / N_0 for n_n in n_ns]
+ax.plot(n_ns, n_ps, "x")
+ax.set_xlabel("n_n")
+ax.set_ylabel("n_p")
 plt.show()
 exit()
-
-#n_ps = np.linspace(4, 11, 10)
-#n_ns = [solve_for_neutron_number_density(.16 * n_p) for n_p in n_ps]
-#plt.plot(n_ps, n_ns)
-n_ns = np.linspace(1, 21, 10)
-n_ps = [solve_for_proton_number_density(.16 * n_n) for n_n in n_ns]
-# plt.plot(n_ps, n_ns)
-plt.show()
-
-exit()
-
-def energy_density_gas(mu, mass):
-    b = sqrt(mu**2 - mass**2)
-    epsilon = (b * mu * (2. * b**2 + mass**2)
-               - mass**4 * arcsinh(b / mass)) / 8.
-    return epsilon / pi**2
-
-def number_density(mu, mass):
-    return sqrt(mu**2 - mass**2)**3 / (3 * pi**2)
-
-def chemical_potential(mass, momentum):
-    return sqrt(mass * mass + momentum * momentum)
 
 def equation_of_state(fermi_momentum):
     '''Calculate the equation of state of an electrically neutral Fermi gas of
@@ -197,7 +215,6 @@ kF1 = 0.01
 k_F_range = np.concatenate([np.linspace(0, kF1, 400), np.linspace(kF1, 100, 100)])
 df = pd.DataFrame.from_records(equation_of_state(k_F) for k_F in k_F_range)
 #n = n_n + n_p
-n_0 = 0.16
 
 plt.plot(df["mu_n"] * MEV_FM, df["n_p"] / (df["n_p"] + np.nan_to_num(df["n_n"])))
 # against baryon density (?)
